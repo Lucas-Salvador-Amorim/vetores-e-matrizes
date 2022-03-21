c6bced9c695cf0161e73ea3ed8e364b6307de3cc
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-03-21 17:48:30 +0000

    tabuuada

---
+++ tabuada.java
@@ -0,0 +1,16 @@
+public class tabuada {
+    public static void main(String[] args){
+        int idades[] = new int[11];
+        
+        for(int i = 0; i < idades.length; i++){
+            idades[i] = i*10;
+        }
+        //for(int i = 0; i < idades.length; i++){ //esse metodo tbm fuinciona
+            //System.out.println(idades[i]); // esse metodo tamvem funciona
+            for(int i:idades){
+                System.out.println(i);
+            }
+        //} //fechando o metodo
+
+    }
+}
